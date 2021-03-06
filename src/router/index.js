ce92720258c50bd702c37bfe2e8ec0cb0840f7d1
Sariful Islam <sarifulislam.molla@gmail.com>
2021-03-07 02:23:36 +0530

    initialised

---
+++ src/router/index.js
@@ -0,0 +1,26 @@
+import {
+  createRouter,
+  createWebHistory
+} from '@ionic/vue-router';
+import {
+  RouteRecordRaw
+} from 'vue-router';
+import Home from '../views/Home.vue';
+
+const routes = [{
+    path: '/',
+    redirect: '/home'
+  },
+  {
+    path: '/home',
+    name: 'Home',
+    component: Home
+  }
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+});
+
+export default router;
